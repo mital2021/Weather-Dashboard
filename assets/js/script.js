ee5e3f7b4016e657982ff0476f12be64dfa8e1ed
mital2021 <87838938+mital2021@users.noreply.github.com>
2021-10-03 14:35:14 -0400

    card title header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 
 
 //search button ,input function
-function myFunction(){
 
-$("#searchButton").on("click", function () {
-var searchTerm = $("#searchValue").val();
-$("#searchValue").val("");
- weatherFunction(searchTerm);
- weatherForecast(searchTerm);
+ $(document).ready(function () {
+
+    $("#searchButton").on("click", function () {
+    var searchTerm = $("#searchValue").val();
+    $("#searchValue").val("");
+    weatherFunction(searchTerm);
+    weatherForecast(searchTerm);
+
 });
 
 
@@ -36,20 +38,48 @@ $("#searchValue").val("");
       fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=cb4df67a69d2950ccf15e89555ac99d4"
         )
+
         .then(function(response){
-            return response.json()
-        })
+            
+       
+        if (history.indexOf(searchTerm) === -1) {
+          
+            history.push(searchTerm);
+        
+            localStorage.setItem("history", JSON.stringify(history));
+            createRow(searchTerm);
+        }
 
-        .then(function(response) {
-            console.log(response.data[0]);
-  });
+        $("#todayWeather").empty();
+
+        var title = $("<h3>").addClass("title").text(response.name + " (" + new Date().toLocaleDateString() + ")");
+        var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+  
+        var card = $("<div>").addClass("card");
+        var cardBody = $("<div>").addClass("card-body");
+        var wind = $("<p>").addClass("card-text").text("Wind Speed: " + response.wind.speed + " MPH");
+        var humid = $("<p>").addClass("card-text").text("Humidity: " + response.main.humidity + " %");
+        var temp = $("<p>").addClass("card-text").text("Temperature: " + response.main.temp + " K");
+        console.log(response)
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
+
+
+
+        
+
+});
   }
-
-
-
-
+     
+  function weatherForecast(searchTerm){
 
 }
+
+
+
+
+
+});
 
 
 
