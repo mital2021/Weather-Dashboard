79d575cf918b69004b2a80f5b52fa809ac570768
mital2021 <87838938+mital2021@users.noreply.github.com>
2021-10-03 10:22:00 -0400

    added more code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,37 @@ function myFunction(){
 
 $("#searchButton").on("click", function () {
 var searchTerm = $("#searchValue").val();
-$("#search-value").val("");
-weatherFunction(searchTerm);
-weatherForecast(searchTerm);
+$("#searchValue").val("");
+ weatherFunction=(searchTerm);
+ weatherForecast=(searchTerm);
 });
 
+
+//local storage, make row to list searched cities
   var history = JSON.parse(localStorage.getItem("history")) || [];
 
+  if (history.length > 0) {
+    weatherFunction(history[history.length - 1]);
+  }
+
+  for (var i = 0; i < history.length; i++) {
+    createRow(history[i]);
+  }
+
+  function createRow(text) {
+    var listItem = $("<li>").addClass("list-group-item").text(text);
+    $(".history").append(listItem);
+  }
+
+  $(".history").on("click", "li", function () {
+    weatherFunction($(this).text());
+    weatherForecast($(this).text());
+  });
 
 
 
 
-
-
-
-
-
+  
 
 
 
