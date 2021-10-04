0dcb3a77eedb7e64e194292d6de225857f391460
mital2021 <87838938+mital2021@users.noreply.github.com>
2021-10-03 21:51:44 -0400

    API is not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,62 @@
 
-
-//search button ,input function
-
- $(document).ready(function () {
-
+//search button feature
+$(document).ready(function () {
+    
     $("#searchButton").on("click", function () {
-    var searchTerm = $("#searchValue").val();
-    $("#searchValue").val("");
-    weatherFunction(searchTerm);
-    weatherForecast(searchTerm);
+      
+      var searchTerm = $("#searchValue").val();
 
-});
+      $("#searchValue").val("");
+      currentWeather(searchTerm);
+      currentForecast(searchTerm);
+    });
 
-
-//local storage, make row to list searched cities
-  var history = JSON.parse(localStorage.getItem("history")) || [];
-
-  if (history.length > 0) {
-    weatherFunction(history[history.length - 1]);
-  }
-
-  for (var i = 0; i < history.length; i++) {
-    createRow(history[i]);
-  }
-
-  function createRow(text) {
-    var listItem = $("<li>").addClass("list-group-item").text(text);
-    $(".history").append(listItem);
-  }
-
-  $(".history").on("click", "li", function () {
-    weatherFunction($(this).text());
-    weatherForecast($(this).text());
-  });
-
-  function weatherFunction(searchTerm){
-      fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=cb4df67a69d2950ccf15e89555ac99d4"
-        )
-
-        .then(function(response){
-            
-       
-        if (history.indexOf(searchTerm) === -1) {
-          
-            history.push(searchTerm);
-        
-            localStorage.setItem("history", JSON.stringify(history));
-            createRow(searchTerm);
-        }
-
-        $("#todayWeather").empty();
-
-        var title = $("<h3>").addClass("title").text(response.name + " (" + new Date().toLocaleDateString() + ")");
-        var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+  //local storage
+      var getHistory = JSON.parse(localStorage.getItem("getHistory")) || [];
+      
+      for (var i = 0; i < getHistory.length; i++) {
+      createRow(getHistory[i]);
+    }
+    function createRow(text) {
+      var listItem = $("<li>").addClass("list-group-item").text(text);
+      $(".getHistory").append(listItem);
+     
+    }
   
+    $(".getHistory").on("click", "li", function () {
+      currentWeather($(this).text());
+      currentForecast($(this).text());
+    });
+  
+ //fetch current weather info
+    function currentWeather (searchTerm) {
+  
+      fetch(
+          "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&units=imperial&appid=cb4df67a69d2950ccf15e89555ac99d4"
+  
+      )
+      .then(function (data) {
+        
+        if (getHistory.indexOf(searchTerm) === -1) {
+         
+          getHistory.push(searchTerm);
+        
+          localStorage.setItem("getHistory", JSON.stringify(getHistory));
+          createRow(searchTerm);
+        }
+       
+        $("#today").empty();
+  
+        var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
         var card = $("<div>").addClass("card");
         var cardBody = $("<div>").addClass("card-body");
-        var wind = $("<p>").addClass("card-text").text("Wind Speed: " + response.wind.speed + " MPH");
-        var humid = $("<p>").addClass("card-text").text("Humidity: " + response.main.humidity + " %");
-        var temp = $("<p>").addClass("card-text").text("Temperature: " + response.main.temp + " K");
-        console.log(response)
-        var lon = response.coord.lon;
-        var lat = response.coord.lat;
+        var wind = $("<p>").addClass("card-text").text("Wind:" + data.wind + " MPH");
+        var humid = $("<p>").addClass("card-text").text("Humidity: " + data.humid + " %");
+        var temp = $("<p>").addClass("card-text").text("Temp: " + data.temp + " F");
+     console.log(data);
 
-
-
-        
+      }); 
+  
+  };
 
 });
-  }
-     
-  function weatherForecast(searchTerm){
-
-}
-
-
-
-
-
-});
-
-
-
-
-
-
-
